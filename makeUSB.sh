dc25fd17c03693d1b8881af0c74f7c917b44ffea
Takumi <125882337+s0racat@users.noreply.github.com>
2025-01-15 13:49:57 +0900

    Update makeUSB.sh

--- makeUSB.sh
+++ makeUSB.sh
@@ -12,7 +12,6 @@ set -o errexit
 
 # Defaults
 scriptname=$(basename "$0")
-eficonfig=1
 data_part=2
 data_fmt="vfat"
 data_size=""
@@ -21,6 +20,8 @@ data_mnt=""
 data_subdir="boot"
 tmp_dir="${TMPDIR-/tmp}"
 update_only=0
+
+cd "$(dirname "$(realpath "$0")")"
 
 # Show usage
 showUsage() {
@@ -31,12 +32,11 @@ showUsage() {
 		 device                         Device to modify (e.g. /dev/sdb)
 		 fs-type                        Filesystem type for the data partition [ext3|ext4|vfat|exfat|ntfs]
 		 data-size                      Data partition size (e.g. 5G)
-		  -e,  --efi                    Enable EFI compatibility
 		  -h,  --help                   Display this message
 		  -s,  --subdirectory <NAME>    Specify a data subdirectory (default: "boot")
 		  -u,  --update                 Only update bootloader and configuration files
 
-		Example: makeUSB.sh -e /dev/sda vfat
+		Example: makeUSB.sh /dev/sda vfat
 
 	EOF
 }
@@ -86,10 +86,6 @@ normal_user="${SUDO_USER-$(id -un)}"
 # Check arguments
 while [ "$#" -gt 0 ]; do
 	case "$1" in
-	-e | --efi)
-		eficonfig=1
-		data_part=2
-		;;
 	-s | --subdirectory)
 		shift && data_subdir="$1"
 		;;
@@ -167,9 +163,8 @@ if [ "$update_only" -eq 0 ]; then
 	#sgdisk --mbrtogpt "$usb_dev" || cleanUp 10
 
 	# Create EFI System partition (50M)
-	[ "$eficonfig" -eq 1 ] &&
-		{ sgdisk --new 1::+50M --typecode 1:700 \
-			--change-name 1:"EFI System" "$usb_dev" || cleanUp 10; }
+	sgdisk --new 1::+50M --typecode 1:700 \
+			--change-name 1:"EFI System" "$usb_dev" || cleanUp 10; 
 
 	# Set data partition size
 	[ -z "$data_size" ] ||
@@ -200,16 +195,14 @@ if [ "$update_only" -eq 0 ]; then
 	unmountUSB "$usb_dev"
 
 	# Set bootable flag for data partion
-	sgdisk --attributes ${data_part}:set:2 "$usb_dev" || cleanUp 10
+	#sgdisk --attributes ${data_part}:set:2 "$usb_dev" || cleanUp 10
 
 	# Unmount device
 	unmountUSB "$usb_dev"
 
 	# Format EFI System partition
-	if [ "$eficonfig" -eq 1 ]; then
-		wipefs -af "${usb_dev}1" || true
-		mkfs.vfat -v -F 32 -n EFI "${usb_dev}1" || cleanUp 10
-	fi
+	wipefs -af "${usb_dev}1" || true
+	mkfs.vfat -v -F 32 -n EFI "${usb_dev}1" || cleanUp 10
 
 	# Wipe data partition
 	wipefs -af "${usb_dev}${data_part}" || true
@@ -231,37 +224,33 @@ efi_mnt=$(mktemp -p "$tmp_dir" -d efi.XXXX) || cleanUp 10
 data_mnt=$(mktemp -p "$tmp_dir" -d data.XXXX) || cleanUp 10
 
 # Mount EFI System partition
-[ "$eficonfig" -eq 1 ] &&
-	{ mount "${usb_dev}1" "$efi_mnt" || cleanUp 10; }
+mount "${usb_dev}1" "$efi_mnt" || cleanUp 10; 
 
 # Mount data partition
 mount "${usb_dev}${data_part}" "$data_mnt" || cleanUp 10
 
 # Install GRUB for EFI
-[ "$eficonfig" -eq 1 ] &&
-	{ $grubefi --target=x86_64-efi --efi-directory="$efi_mnt" \
+$grubefi --target=x86_64-efi --efi-directory="$efi_mnt" \
 		--boot-directory="${data_mnt}/${data_subdir}" --removable --recheck ||
-		cleanUp 10; }
+		cleanUp 10; 
 
 # Create necessary directories
 mkdir -p "${data_mnt}/${data_subdir}/isos" || cleanUp 10
 
 # Copy files
-(cd "$(dirname "$(realpath "$0")")" && cp -R ./mbusb.* "${data_mnt}/${data_subdir}"/grub*/) ||
+cp -R ./mbusb.* "${data_mnt}/${data_subdir}"/grub*/ ||
 	cleanUp 10
 
-(cd "$(dirname "$(realpath "$0")")" && sed -i "1aset rootuuid=$(blkid -o value -s UUID ${usb_dev}${data_part})" ${data_mnt}/${data_subdir}/grub/mbusb.cfg) ||
+sed -i "1aset rootuuid=$(blkid -o value -s UUID ${usb_dev}${data_part})" ${data_mnt}/${data_subdir}/grub/mbusb.cfg) ||
 	cleanUp 10
 
 # Copy example configuration for GRUB
-(cd "$(dirname "$(realpath "$0")")" && cp ./grub.cfg.example "${data_mnt}/${data_subdir}"/grub*/) ||
+cp ./grub.cfg.example "${data_mnt}/${data_subdir}"/grub*/ ||
 	cleanUp 10
 
 # Rename example configuration
-(cd "${data_mnt}/${data_subdir}"/grub*/ && cp grub.cfg.example grub.cfg) ||
+(cd "${data_mnt}/${data_subdir}"/grub*/ && cp grub.cfg.example grub.cfg && mkdir tools) ||
 	cleanUp 10
-
-(cd "${data_mnt}/${data_subdir}"/grub*/ && mkdir tools ) || cleanUp 10
 
 # Download wimboot
 wimboot_url='https://gitlab.com/api/v4/projects/55131919/packages/generic/wimboot/v2.8.0-1/wimboot-v2.8.0-1.tar.gz'
